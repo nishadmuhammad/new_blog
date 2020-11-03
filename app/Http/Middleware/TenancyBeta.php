bc642addbbb6d89012e15708da619577b29f8484
nishadmuhammad <nishadmuhammad635@gmail.com>
2020-11-03 19:25:45 +0530

    TenancyBeta modified

--- app/Http/Middleware/TenancyBeta.php
+++ app/Http/Middleware/TenancyBeta.php
@@ -17,16 +17,7 @@ class TenancyBeta
     {
              // echo"hello testing now";
      //   Check if its first time visit enable shared session
-        if ($request->has('session_token')) {
-            $user = Auth::user();
-            Session::setId($request->get('session_token'));
-            Session::start();
-        }
-        // Check if session doesn't exist
-        elseif (count(Session::all()) == 0) {
-
-            return redirect()->route('master.logout', ['accounts']);
-        }
+    
         // Get the subdomain and modify the database on the fly
         $route = $request->route();
         $subdomain = $route->parameter('subdomain');
@@ -42,17 +33,6 @@ class TenancyBeta
         Config::set('database.default', 'mysql');
         DB::reconnect('mysql');
         // Store the credentials into the session
-        $request->session()->put('tenant', $tenant);
-        $request->session()->put($subdomain, $tenant);
-        $request->session()->put('subdomain', $subdomain);
-        $request->session()->put('auth_users', Candidate::all_user());
-        $response = $next($request);
-        $user = GlobalAuth::user();
-        if ($user) {
-            $response->headers->set('X-Username', $user->email);
-        } else {
-            $response->headers->set('X-Username', 'Anonymous');
-        }
-        // return $next($request);
+        return $next($request);
     }
 }
